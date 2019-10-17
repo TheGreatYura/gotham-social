32615e1b758fe44f42f3371eeeb2324528dc64b9
TheGreatYura <minenokyura@gmail.com>
2019-10-17 12:35:40 +0300

    Updates

---
+++ precache-manifest.9eb8d71cb01ade0eaad5e8abf56d91c9.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6387708f104b767538995e5ce059d075",
+    "url": "/gotham-social/index.html"
+  },
+  {
+    "revision": "12bcc3ab2db154ac36a7",
+    "url": "/gotham-social/static/css/3.a4220a73.chunk.css"
+  },
+  {
+    "revision": "c247cd461e01d794f392",
+    "url": "/gotham-social/static/css/4.8eaecea9.chunk.css"
+  },
+  {
+    "revision": "419891b1ef17b2b49c53",
+    "url": "/gotham-social/static/css/5.eb20a7dc.chunk.css"
+  },
+  {
+    "revision": "9bfdd32387476685c047",
+    "url": "/gotham-social/static/css/main.bb450251.chunk.css"
+  },
+  {
+    "revision": "8cfb27e392a30e7e07c3",
+    "url": "/gotham-social/static/js/2.815e279d.chunk.js"
+  },
+  {
+    "revision": "12bcc3ab2db154ac36a7",
+    "url": "/gotham-social/static/js/3.46e5a162.chunk.js"
+  },
+  {
+    "revision": "c247cd461e01d794f392",
+    "url": "/gotham-social/static/js/4.0c9218e8.chunk.js"
+  },
+  {
+    "revision": "419891b1ef17b2b49c53",
+    "url": "/gotham-social/static/js/5.6dea7321.chunk.js"
+  },
+  {
+    "revision": "9bfdd32387476685c047",
+    "url": "/gotham-social/static/js/main.128dc88e.chunk.js"
+  },
+  {
+    "revision": "24c9fd9170dc58da6568",
+    "url": "/gotham-social/static/js/runtime-main.b08f700f.js"
+  },
+  {
+    "revision": "6e73399070fb13fc42d740e85ba50a4d",
+    "url": "/gotham-social/static/media/Eclipse.6e733990.svg"
+  },
+  {
+    "revision": "1a93248363c00b1e9a8631c89bc86eda",
+    "url": "/gotham-social/static/media/icon.1a932483.jpg"
+  },
+  {
+    "revision": "02322427a9c0cd15b4cdcc948dce8894",
+    "url": "/gotham-social/static/media/logo.02322427.png"
+  }
+]);
